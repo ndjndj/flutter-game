39b58956f5dbff8e9b2e3cbd2654daaa3f3f3af6
ndjndj <teq.ndj@gmail.com>
2023-03-23 00:48:47 +0900

    f

--- hello/lib/game/block_breaker.dart
+++ hello/lib/game/block_breaker.dart
@@ -1,3 +1,20 @@
 import "package:flame/game.dart";
 
-class BlockBreaker extends FlameGame {}
+import "package:hello/constants/constants.dart";
+import "package:hello/game/component/paddle.dart";
+
+class BlockBreaker extends FlameGame {
+  @override
+  Future<void>? onLoad() async {
+    final paddle = Paddle();
+    final paddleSize = paddle.size;
+
+    paddle
+    ..position.x = size.x / 2 - paddleSize.x / 2
+    ..position.y = size.y - paddleSize.y - kPaddleStartY;
+
+    await addAll([
+      paddle
+    ]);
+  }
+}
