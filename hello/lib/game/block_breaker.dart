fa95e0870c5477f297643cf3f105cfc67c136709
ndjndj <teq.ndj@gmail.com>
2023-03-23 01:02:15 +0900

    f

--- hello/lib/game/block_breaker.dart
+++ hello/lib/game/block_breaker.dart
@@ -30,4 +30,34 @@ class BlockBreaker extends FlameGame {
     await add(ball);
   }
 
+  Future<void> resetBlock() async {
+    final sizeX = (
+      size.x - kBlocksStartXPosition * 2 - kBlockPadding * (kBlocksRowCount - 1)
+    ) / kBlocksRowCount;
+
+    final sizeY = (
+      size.y * kBlocksHeightRatio - kBlocksStartYPosition - kBlockPadding + (kBlocksColumnCount - 1)
+    ) / kBlocksColumnCount;
+
+    final blocks = List<Block>.generate(
+      kBlocksColumnCount * kBlocksRowCount,
+      (int index) {
+        final block = Block(
+          blockSize:  Vector2(sizeX, sizeY)
+        );
+
+        final indexX = index % kBlocksRowCount;
+        final indexY = index ~/ kBlocksRowCount;
+
+        block.position
+        ..x = kBlocksStartXPosition + indexX * (block.size.x + kBlockPadding)
+        ..y = kBlocksStartYPosition + indexY * (block.size.y + kBlockPadding);
+
+        return block;
+      }
+    );
+
+    await addAll(blocks);
+  }
+
 }
