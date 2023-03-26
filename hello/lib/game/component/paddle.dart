e522d12d6c81d6dbc349c5cfffef54be780e4260
ndjndj <teq.ndj@gmail.com>
2023-03-27 03:51:58 +0900

    f

--- hello/lib/game/component/paddle.dart
+++ hello/lib/game/component/paddle.dart
@@ -1,13 +1,24 @@
 import "dart:ui";
 
 import "package:flame/components.dart";
+import 'package:flame/experimental.dart';
 
 import "package:hello/constants/constants.dart";
 
-class Paddle extends RectangleComponent {
-  Paddle()
+class Paddle extends RectangleComponent with DragCallbacks {
+  Paddle({
+    required this.draggingPaddle
+  })
   : super(
     size: Vector2(kPaddleWidth, kPaddleHeight),
     paint: Paint()..color = kPaddleColor
   );
+
+  final void Function(DragUpdateEvent event) draggingPaddle;
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    draggingPaddle(event);
+    super.onDragUpdate(event);
+  }
 }
