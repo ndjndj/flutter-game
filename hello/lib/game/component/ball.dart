abd9372d5cc841fe45ee488badb56ffc67c676fb
ndjndj <teq.ndj@gmail.com>
2023-03-27 04:03:36 +0900

    f

--- hello/lib/game/component/ball.dart
+++ hello/lib/game/component/ball.dart
@@ -1,11 +1,13 @@
+import 'dart:async';
 import "dart:ui";
 import "dart:math";
 
+import 'package:flame/collisions.dart';
 import "package:flame/components.dart";
 
 import "package:hello/constants/constants.dart";
 
-class Ball extends CircleComponent {
+class Ball extends CircleComponent with CollisionCallbacks {
   Ball() {
     radius = kBallRadius;
     paint = Paint()..color = kBallColor;
@@ -19,6 +21,13 @@ class Ball extends CircleComponent {
   double get spawnAngle {
     final random = Random().nextDouble();
     return lerpDouble(kBallMinSpawnAngle, kBallMaxSpawnAngle, random)!;
+  }
+
+  @override
+  Future<void> onLoad() async {
+    final hitbox = CircleHitbox(radius: radius);
+    await add(hitbox);
+    return super.onLoad();
   }
 
   @override
