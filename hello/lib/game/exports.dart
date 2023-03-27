21a3f74eb4e4b0311876987ed702251255252ea3
ndjndj <teq.ndj@gmail.com>
2023-03-28 00:14:54 +0900

    f

--- hello/lib/game/exports.dart
+++ hello/lib/game/exports.dart
@@ -1,3 +1,5 @@
 export "package:hello/game/component/ball.dart";
 export "package:hello/game/component/paddle.dart";
 export "package:hello/game/component/block.dart";
+export "package:hello/game/component/countdown_text.dart";
+export "package:hello/game/component/my_text_button.dart";
